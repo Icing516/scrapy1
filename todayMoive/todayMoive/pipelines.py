7a558429bb0ed459f67cd92abbf07f9ba680208c
Icing <fangxin516@163.com>
2017-02-24 18:59:27 +0800

    爬虫scrapy

--- todayMoive/todayMoive/pipelines.py
+++ todayMoive/todayMoive/pipelines.py
@@ -5,7 +5,16 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import time
+
 #定义爬取的内容最终怎么处理
 class TodaymoivePipeline(object):
-    def process_item(self, item, spider):
+    def process_item(self, items, spider):
+        # now = time.strftime('%Y-%m-%d', time.localtime())
+        # fileName = 'wuHan' + now + '.txt'
+        fileName = './todayMoive.txt'
+        with open(self.fileName, 'w') as fp:
+            for item in items:
+                item = str(item['filmName'])
+                fp.write(item + '\n')
         return item
--- todayMoive/todayMoive/spiders/moiveSpidler.py
+++ todayMoive/todayMoive/spiders/moiveSpidler.py
@@ -5,24 +5,52 @@ from todayMoive.items import TodaymoiveItem
 from scrapy.spiders import CrawlSpider
 from scrapy.http import Request
 from scrapy.selector import Selector
+from scrapy.http import Request, FormRequest
+import json
 
 class moiveSpidler(CrawlSpider):
     name = 'moiveSpidler'
-    allowed_domains = ["jycinema,com"]
-    start_urls = ('http://www.jycinema.com/frontUIWebapp/appserver/cinCinemaFilmViewService/findFilm',)
-    head = {'Host': 'www.jycinema.com',
+    allowed_domains = ["www.jycinema.com"]
+    start_urls = ('http://www.jycinema.com/frontUIWebapp/appserver/cinCinemaFilmViewService/findFilm')
+    headers = {'Host': 'www.jycinema.com',
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
 
-    param = {
-        'params': '''{"type":"queryFilm","cityName":"深圳市","cinemaId":"","statusRE":"RELEASE","channelCode":"J0002","channelId":"3"}'''}
+    # def start_requests(self):
+    #     return Request(self.start_urls,
+    #                     headers=self.headers,
+    #                     meta ={'params': '''{"type":"queryFilm","cityName":"深圳市","cinemaId":"","statusRE":"RELEASE","channelCode":"J0002","channelId":"3"}'''},
+    #                     callback=self.parse)
+
+
+    def start_requests(self):
+        print '开始爬虫。。。'
+        # param = {'params': '''{"type":"queryFilm","cityName":"深圳市","cinemaId":"","statusRE":"RELEASE","channelCode":"J0002","channelId":"3"}'''}
+        return [FormRequest(url=self.start_urls,  # "http://www.zhihu.com/login",
+                            # meta={'cookiejar': response.meta['cookiejar']},
+                            headers=self.headers,  # 注意此处的headers
+                            formdata={'params':'{"type":"queryFilm","cityName":"深圳市","cinemaId":"","statusRE":"RELEASE","channelCode":"J0002","channelId":"3"}'},
+                            callback=self.parse,
+                            )]
 
     def parse(self, response):
-        subSelecter = response.content
+        print 'response=',response
+        subSelecter = Selector(response)
+        # subSelecter = response.content
+        print '获取电影数据。。。'
         print subSelecter
         items =[]
-        for sub in subSelecter:
+        json_result = json.loads(subSelecter)
+        items = json_result['data']
+
+        for item in items:
             item = TodaymoiveItem()
-            item['moiveName'] = sub.xpath('').extract()
+            item['moiveName'] = str(item['filmName'])
             items.append(item)
+            print item['filmName']
+
+        # for sub in subSelecter:
+        #     item = TodaymoiveItem()
+        #     item['moiveName'] = sub.xpath('').extract()
+        #     items.append(item)
         return items
